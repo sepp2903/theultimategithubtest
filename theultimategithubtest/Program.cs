9903587651e51d880d0712bb7795948e4b70f83f
sepp2903 <114078159+sepp2903@users.noreply.github.com>
2022-09-21 16:02:31 +0200

    scheiß die wand an

--- theultimategithubtest/Program.cs
+++ theultimategithubtest/Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Wehe der scheiß funktioniert jetzt nicht");
+Console.WriteLine("scheiß die wand an, es funktioniert");
